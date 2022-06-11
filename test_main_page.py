7ae0af39f39b01ef0b3b267226e507af8760e1b1
au52 <104981778+au52@users.noreply.github.com>
2022-06-11 11:54:38 +0300

    Add files via upload
    Import new files

--- test_main_page.py
+++ test_main_page.py
@@ -1,19 +1,9 @@
-import pytest
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-import time
-import math
-
+from .pages.main_page import MainPage
 
 # Строка для запуска: pytest -v --tb=line --language=en test_main_page.py
 
-def go_to_login_page(browser):
-    login_link = browser.find_element(By.CSS_SELECTOR, "#login_link")
-    login_link.click()
-
 def test_guest_can_go_to_login_page(browser):
     link = "http://selenium1py.pythonanywhere.com/"
-    browser.get(link)
-    go_to_login_page(browser)
+    page = MainPage(browser, link)   # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
+    page.open()                      # открываем страницу
+    page.go_to_login_page()          # выполняем метод страницы — переходим на страницу логина
